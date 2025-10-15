042d66ce3d6ddd77f02d9d7acfe3262a780cd12c
riririmmm <mr8774@smail.kongju.ac.kr>
2025-10-15 09:37:56 +0900

    second commit

---
+++ lect03/exec2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Parent process\n");
+	if (fork() == 0) {
+		printf("Child process\n");
+		execl("/bin/echo", "echo", "hello", NULL);
+		printf("End of child process \n");
+	}
+
+	printf("End of parent proces\n");
+	return 0;
+}
+
